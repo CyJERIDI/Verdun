49309827747ec1b2afca28358e815b8c296f0999
CyJERIDI <132550570+CyJERIDI@users.noreply.github.com>
2023-05-04 16:16:57 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -172,4 +172,12 @@ st.pyplot(fig)
 
 dat = st.text_input("Faire la prédiction à partir de la date : ", '') 
 
+if dat:
+ d=predict(dat)
+ st.dataframe(d['yhat'])
+ chart_data = pd.DataFrame(d[[ 'ds','yhat' ]] )
+ st.line_chart(chart_data , x='ds', y='yhat' )
+
+
+
 
